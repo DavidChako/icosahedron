a3c5019495a089878af9005434212aa1f161c076
davidchako <dchako@icosahedron.com>
2021-04-10 15:07:34 -0400

    Factored up fibonacci to Sequence companion

--- src/main/kotlin/com/icosahedron/math/Math.kt
+++ src/main/kotlin/com/icosahedron/math/Math.kt
@@ -1,17 +1,17 @@
-package com.icosahedron.math.errata
+package com.icosahedron.math
 
 import com.icosahedron.format.Format
 
 fun main() {
-    println("Hello Errata!")
+    println("Hello Math!")
     printFibonnaciNumbers()
 }
 
 fun printFibonnaciNumbers() {
     val title = "Fibonacci numbers"
     println(Format.blockBegin(title))
-    (0 until 10).forEach { println("fibonacci($it) = ${Fibonacci.fibonacci(it)}") }
+    (0 until 10).forEach { println("fibonacci($it) = ${Sequence.fibonacci(it)}") }
     println("...")
-    println("fibonacci(10K) = ${Fibonacci.fibonacci(10_000)}")
+    println("fibonacci(10K) = ${Sequence.fibonacci(10_000)}")
     println(Format.blockEnd(title))
 }
