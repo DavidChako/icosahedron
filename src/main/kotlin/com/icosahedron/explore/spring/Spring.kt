a4c6f9506b2d47e3de2ea8cd1a4cce47bddc34ed
davidchako <dchako@icosahedron.com>
2021-04-25 21:34:49 -0400

    Exploring Kotlin features

--- src/main/kotlin/com/icosahedron/explore/spring/Spring.kt
+++ src/main/kotlin/com/icosahedron/explore/spring/Spring.kt
@@ -1,11 +1,4 @@
 package com.icosahedron.explore.spring
 
-data class Customer(var name: String, val email: String, val company: String)
-
 fun main() {
-    val customer = Customer("David Chako", "dchako@icosahedron.com", "Icosahedron Design")
-    println(customer)
-
-    customer.name = "maggie"
-    println(customer)
 }
