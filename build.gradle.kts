352f1635d2b65605080c9778c305d20eedc2360c
davidchako <dchako@icosahedron.com>
2021-05-02 14:05:58 -0400

    Explore spring web services with kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,26 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.5"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.4.32"
-    kotlin("plugin.spring") version "1.4.32"
     groovy
 }
 
 group = "com.icosahedron"
 version = "1.0-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("com.h2database:h2")
     testImplementation("org.spockframework:spock-core:2.0-M3-groovy-3.0")
 }
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
