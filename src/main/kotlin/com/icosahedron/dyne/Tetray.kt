7683ddd4500813472778d9d3a3b07a5959c316c3
davidchako <dchako@icosahedron.com>
2021-08-22 10:26:48 -0400

    crafted Tetray simplified

--- src/main/kotlin/com/icosahedron/dyne/Tetray.kt
+++ src/main/kotlin/com/icosahedron/dyne/Tetray.kt
@@ -1,37 +1,38 @@
 package com.icosahedron.dyne
 
-data class Tetray(private var x0: ULong, private var x1: ULong, private var x2: ULong, private var x3: ULong) {
-    var shell = x0 + x1 + x2 + x3
+class Tetray private constructor(private val array: Array<ULong>) {
+    var shell = array.sum()
         private set
-        get() = field
+
+    constructor(x0: ULong, x1: ULong, x2: ULong, x3: ULong): this(arrayOf(x0, x1, x2, x3))
 
     constructor(x0: Int, x1: Int, x2: Int, x3: Int): this(x0.toULong(), x1.toULong(), x2.toULong(), x3.toULong())
 
-    override fun toString() = "$x0:$x1:$x2:$x3"
-
-    operator fun get(i: Int) = coordinate(i).get()
+    operator fun get(i: Int) = array[i]
 
     fun increment(i: Int): Tetray {
-        val coordinate = coordinate(i)
-        val result = coordinate.get() + 1UL
-        coordinate.set(result)
-        shell++
+        require(array[i] < ULong.MAX_VALUE)
+        array[i] = array[i] + 1UL
+        shell += 1UL
         return this
     }
 
     fun decrement(i: Int): Tetray {
-        val coordinate = coordinate(i)
-        val result = coordinate.get() - 1UL
-        coordinate.set(result)
-        shell--
+        require(array[i] > ULong.MIN_VALUE)
+        array[i] = array[i] - 1UL
+        shell -= 1UL
         return this
     }
 
-    private fun coordinate(i: Int) = when (i) {
-        0 -> this::x0
-        1 -> this::x1
-        2 -> this::x2
-        3 -> this::x3
-        else -> throw ArrayIndexOutOfBoundsException(i)
+    override fun toString() = array.joinToString(":")
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return array.contentEquals((other as Tetray).array)
+    }
+
+    override fun hashCode(): Int {
+        return array.contentHashCode()
     }
 }
--- src/test/kotlin/com/icosahedron/dyne/TetrayTest.kt
+++ src/test/kotlin/com/icosahedron/dyne/TetrayTest.kt
@@ -1,120 +1,90 @@
 package com.icosahedron.dyne
 
+import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotEquals
+import kotlin.test.assertNotSame
 
 class TetrayTest {
-    @Test fun `construct from ULongs`() {
-        // given
-        val x0 = Arbitrary.unsignedLong()
-        val x1 = Arbitrary.unsignedLong()
-        val x2 = Arbitrary.unsignedLong()
-        val x3 = Arbitrary.unsignedLong()
+    private val x0 = Random.nextInt(1, Int.MAX_VALUE).toULong()
+    private val x1 = Random.nextInt(1, Int.MAX_VALUE).toULong()
+    private val x2 = Random.nextInt(1, Int.MAX_VALUE).toULong()
+    private val x3 = Random.nextInt(1, Int.MAX_VALUE).toULong()
+
+    @Test fun `shell property get`() {
         val tetray = Tetray(x0, x1, x2, x3)
+        assertEquals(x0 + x1 + x2 + x3, tetray.shell)
+    }
 
-        // when
-        val actual = listOf(tetray[0], tetray[1], tetray[2], tetray[3])
-
-        // then
-        val expected = listOf(x0, x1, x2, x3)
-        assertEquals(expected, actual)
+    @Test fun `construct from ULongs and get at index`() {
+        val tetray = Tetray(x0, x1, x2, x3)
+        assertEquals(tetray[0], x0)
+        assertEquals(tetray[1], x1)
+        assertEquals(tetray[2], x2)
+        assertEquals(tetray[3], x3)
     }
 
     @Test fun `construct from Ints`() {
-        // given
-        val x0 = Arbitrary.integer()
-        val x1 = Arbitrary.integer()
-        val x2 = Arbitrary.integer()
-        val x3 = Arbitrary.integer()
+        val tetray = Tetray(x0.toInt(), x1.toInt(), x2.toInt(), x3.toInt())
+        assertEquals(Tetray(x0, x1, x2, x3), tetray)
+    }
 
-        // when
-        val actual = Tetray(x0, x1, x2, x3)
+    @Test fun `increment at index`() {
+        assertEquals(Tetray(x0 + 1UL, x1, x2, x3), Tetray(x0, x1, x2, x3).increment(0))
+        assertEquals(Tetray(x0, x1 + 1UL, x2, x3), Tetray(x0, x1, x2, x3).increment(1))
+        assertEquals(Tetray(x0, x1, x2 + 1UL, x3), Tetray(x0, x1, x2, x3).increment(2))
+        assertEquals(Tetray(x0, x1, x2, x3 + 1UL), Tetray(x0, x1, x2, x3).increment(3))
+    }
 
-        // then
-        val expected = Tetray(x0.toULong(), x1.toULong(), x2.toULong(), x3.toULong())
-        assertEquals(expected, actual)
+    @Test fun `decrement at index`() {
+        assertEquals(Tetray(x0 - 1UL, x1, x2, x3), Tetray(x0, x1, x2, x3).decrement(0))
+        assertEquals(Tetray(x0, x1 - 1UL, x2, x3), Tetray(x0, x1, x2, x3).decrement(1))
+        assertEquals(Tetray(x0, x1, x2 - 1UL, x3), Tetray(x0, x1, x2, x3).decrement(2))
+        assertEquals(Tetray(x0, x1, x2, x3 - 1UL), Tetray(x0, x1, x2, x3).decrement(3))
     }
 
     @Test fun `to string`() {
-        // given
-        val tetray = Arbitrary.tetray()
-
-        // when
-        val actual = tetray.toString()
-
-        // then
-        val expected = "${tetray[0]}:${tetray[1]}:${tetray[2]}:${tetray[3]}"
-        assertEquals(expected, actual)
-    }
-
-    @Test fun `get shell`() {
-        // given
-        val tetray = Arbitrary.tetray()
-
-        // when
-        val actual = tetray.shell
-
-        // then
-        val expected = tetray[0] + tetray[1] + tetray[2] + tetray[3]
-        assertEquals(expected, actual)
-    }
-
-    @Test fun `get coordinate`() {
-        // given
-        val x0 = Arbitrary.unsignedLong()
-        val x1 = Arbitrary.unsignedLong()
-        val x2 = Arbitrary.unsignedLong()
-        val x3 = Arbitrary.unsignedLong()
-
-        // when
         val tetray = Tetray(x0, x1, x2, x3)
-
-        // then
-        assertEquals(x0, tetray[0])
-        assertEquals(x1, tetray[1])
-        assertEquals(x2, tetray[2])
-        assertEquals(x3, tetray[3])
+        assertEquals("$x0:$x1:$x2:$x3", tetray.toString())
     }
 
-    @Test fun `increment coordinate`() {
-        // given
-        val x0 = Arbitrary.unsignedLong()
-        val x1 = Arbitrary.unsignedLong()
-        val x2 = Arbitrary.unsignedLong()
-        val x3 = Arbitrary.unsignedLong()
-        val initial = List(4) { Tetray(x0, x1, x2, x3) }
-
-        // when
-        val actual = initial.mapIndexed { index, tetray -> tetray.increment(index) }
-
-        // expect
-        val expected = listOf(
-            Tetray(x0 + 1UL, x1, x2, x3),
-            Tetray(x0, x1 + 1UL, x2, x3),
-            Tetray(x0 , x1, x2 + 1UL, x3),
-            Tetray(x0, x1, x2, x3 + 1UL),
-        )
-        assertEquals(expected, actual)
+    @Test fun `equals is reflexive`() {
+        val tetray = Tetray(x0, x1, x2, x3)
+        assertEquals(tetray, tetray)
     }
 
-    @Test fun `decrement coordinate`() {
-        // given
-        val x0 = Arbitrary.unsignedLong() + 1UL
-        val x1 = Arbitrary.unsignedLong() + 1UL
-        val x2 = Arbitrary.unsignedLong() + 1UL
-        val x3 = Arbitrary.unsignedLong() + 1UL
-        val initial = List(4) { Tetray(x0, x1, x2, x3) }
+    @Test fun `equals is symmetric`() {
+        val tetrayA = Tetray(x0, x1, x2, x3)
+        val tetrayB = Tetray(x0, x1, x2, x3)
+        assertNotSame(tetrayA, tetrayB)
+        assertEquals(tetrayA, tetrayB)
+        assertEquals(tetrayB, tetrayA)
+    }
 
-        // when
-        val actual = initial.mapIndexed { index, tetray -> tetray.decrement(index) }
+    @Test fun `equals is transitive`() {
+        val tetrayA = Tetray(x0, x1, x2, x3)
+        val tetrayB = Tetray(x0, x1, x2, x3)
+        val tetrayC = Tetray(x0, x1, x2, x3)
+        assertNotSame(tetrayA, tetrayB)
+        assertNotSame(tetrayB, tetrayC)
+        assertEquals(tetrayA, tetrayB)
+        assertEquals(tetrayB, tetrayC)
+        assertEquals(tetrayA, tetrayC)
+    }
 
-        // expect
-        val expected = listOf(
-            Tetray(x0 - 1UL, x1, x2, x3),
-            Tetray(x0, x1 - 1UL, x2, x3),
-            Tetray(x0 , x1, x2 - 1UL, x3),
-            Tetray(x0, x1, x2, x3 - 1UL),
-        )
-        assertEquals(expected, actual)
+    @Test fun `not equals`() {
+        val tetray = Tetray(x0, x1, x2, x3)
+        assertFalse(tetray.equals("foobar"))
+        assertNotEquals(Tetray(x0 + 1UL, x1, x2, x3), tetray)
+        assertNotEquals(Tetray(x0, x1 + 1UL, x2, x3), tetray)
+        assertNotEquals(Tetray(x0, x1, x2 + 1UL, x3), tetray)
+        assertNotEquals(Tetray(x0, x1, x2, x3 + 1UL), tetray)
+    }
+
+    @Test fun `hash code`() {
+        val tetray = Tetray(x0, x1, x2, x3)
+        assertEquals(arrayOf(x0, x1, x2, x3).contentHashCode(), tetray.hashCode())
     }
 }
